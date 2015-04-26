fee69ac70a7e0727cd066294299045a225186332
Kevin Koshy <kevin@logicmash.com>
2015-04-26 21:53:32 +0300

    Adding session functionality along with username and passowrd

--- blog.py
+++ blog.py
@@ -6,6 +6,10 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 import sqlite3
 
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
 
 app = Flask(__name__)
 
@@ -17,13 +21,23 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
-@app.route('/')
-def login()
-    return render_template('login.html')
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username']!=app.config['USERNAME'] or \
+            request.form['password']!=app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error = error)
 
 @app.route('/main')
-def main()
+def main():
     return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
